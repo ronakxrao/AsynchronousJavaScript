15a2cd6f30c66bdfe799b11416542cc0cd3c9890
ronakxrao <yadavronak301@gmail.com>
2024-07-10 11:19:28 +0530

    Changed in index.html

--- callbacks.js
+++ callbacks.js
@@ -1,15 +1,15 @@
-const posts =[
+const posts = [
     { title: 'Post One', body: 'This is post one' },
     { title: 'Post Two', body: 'This is post two' },
 ];
 
 function getPosts() {
     setTimeout(() => {
-    let output = '';
-    posts.forEach((post, index) => {
-    output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
     }, 1000);
 }
 
@@ -20,4 +20,4 @@ function createPost(post, callback) {
     }, 2000);
 }
 
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
